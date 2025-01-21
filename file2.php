b7b6f66da1bc0524173ee905bb1a31fb40bc940d
mmed <mohamed.lahrech.rmd@gmail.com>
2025-01-21 15:52:25 +0100

    change the background for .php

--- file2.php
+++ file2.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            background-color: red;
+        }
+    </style>
 </head>
 <body>
     <?php
         for($i=0; $i<5; $i++)
         echo "<h1>Hello, World!</h1>"; 
     ?>
+
 </body>
 </html>
